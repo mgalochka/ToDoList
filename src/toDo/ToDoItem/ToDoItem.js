b672a547092937cf672b0de6e744ee85db368270
mgalochka <mgalochka5@gmail.com>
2022-02-08 10:08:32 +0200

    addad fixid

--- src/toDo/ToDoItem/ToDoItem.js
+++ src/toDo/ToDoItem/ToDoItem.js
@@ -1,16 +1,19 @@
 import React, {useState} from 'react';
+import RemuvePng from '../../assets/img/remuve.png';
 import "./ToDoItem.css"
 
 
 const ToDoItem = (props) => {
         
     return (
-        <div className="todo-item" >
-            <input type="checkbox" 
+        <div className="todo__item" >
+            <input className='todo__checkbox' type="checkbox" 
             onChange={props.handleChange } 
             defaultChecked={props.completed}></input>
-            <p className="description">{props.description}</p>
-           <button className="dotos__remuve" onClick={props.removeItem}>Удалить</button>
+            <p className="todo__description">{props.description}</p>
+           <a className="dotos__remuve" onClick={props.removeItem}>
+             <img className='todos__img' src={RemuvePng} alt={RemuvePng} />   
+           </a>
            
         </div>
     )
